bfa3af1098baeeee0bfd282299db7a5c785f3dbf
Eman Sarah Afi <85173630+emansarahafi@users.noreply.github.com>
2023-12-07 20:48:44 +0100

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,28 +1,42 @@
-var mongoose = require("mongoose"); 
-const Schema = mongoose.Schema; 
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
 const userSchema = new Schema({
     userName: {
         type: String,
         required: true,
-    }, email: {
-        type: String,
-        required: true,
-    }, age: {
-        type: String,
-        required: true,
-    }, password: {
-        type: String,
-        required: true,
-    }, role: {
-        type: String,
-        enum: ['user','admin'], // Allowed roles
-        default: 'user', // Default role
-    },  profileImagePath: { // Add this field 
-        type: String,
-        required: false
     },
-} );
+    email: {
+        type: String,
+        required: true,
+    },
+    dob: {
+        type: Date,
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'], // Allowed roles
+        default: 'user', // Default role
+    },
+    profileImagePath: { // Add this field
+        type: String,
+        required: false,
+    },
+});
 
-const User = mongoose.model('User',userSchema) 
+const User = mongoose.model('User', userSchema);
 
-module.exports= User;
+module.exports = User;
