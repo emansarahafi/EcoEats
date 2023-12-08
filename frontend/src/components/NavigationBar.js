26e1ba46d456e2e4bea8193d4ece36477176613b
Eman Sarah Afi <85173630+emansarahafi@users.noreply.github.com>
2023-12-08 19:39:12 +0100

    Update NavigationBar.js

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -2,15 +2,24 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { useNavigate } from "react-router-dom";
+import { UserRole } from "./UserRole";
 import { Link, NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBell, faShoppingCart, faHeadset } from "@fortawesome/free-solid-svg-icons";
+import { faBell, faShoppingCart, faHeadset, faFileAlt, faUsers, faUser } from "@fortawesome/free-solid-svg-icons";
 import Badge from 'react-bootstrap/Badge';
 
 
 export default function NavigationBar({cartItemsCount}) {
- 
+  const role = UserRole(); 
+  const navigate=useNavigate() 
 
+  const handleSignOut = () => {
+    if (window.confirm("Are you sure you want to SignOut")){ 
+      localStorage.removeItem("token"); // Remove the token from local storage 
+      navigate("/"); // Navigate to the home page or login page
+    } 
+  };
   return (
     <Navbar >
       <Container>
@@ -26,51 +35,71 @@ export default function NavigationBar({cartItemsCount}) {
             
           }}
         >
-          <Nav.Link>
-            <NavLink
-              to="/notifications"
-              style={({ isActive }) => {
-                return {
-                  
-                  fontWeight: isActive ? "bold" : "",
-                  fontSize: isActive ? "25px" : "20px",
-                };
-              }}
-            >
-              <FontAwesomeIcon icon={faBell}  />
-            </NavLink>
-          </Nav.Link>
+          {role === "user" && (
+            <Nav.Link as={NavLink} to="/notifications">
+              <FontAwesomeIcon icon={faBell} />
+            </Nav.Link>
+          )}
 
           <Nav.Link>
             <NavLink to="/cart"  >
-              <FontAwesomeIcon icon={faShoppingCart}  />  <Badge bg="danger">{cartItemsCount ?? 0}</Badge>
+              <FontAwesomeIcon icon={faShoppingCart}/>  <Badge bg="danger">{cartItemsCount ?? 0}</Badge>
             </NavLink>
           </Nav.Link>
 
-          {/* New NavLink for customer service */}
           <Nav.Link>
             <NavLink
-              to="/customers"
-              style={({ isActive }) => {
-                return {
-               
-                  fontWeight: isActive ? "bold" : "",
-                  fontSize: isActive ? "25px" : "20px",
-                };
-              }}
-            >
+              to="/customers">
               <FontAwesomeIcon icon={faHeadset}  />
             </NavLink>
           </Nav.Link>
 
+          {role === "admin" && (
+            <Nav.Link as={NavLink} to="/customerServices">
+              <FontAwesomeIcon icon={faFileAlt} />
+            </Nav.Link>
+          )}
+
+          
+          {role === "admin" && (
+            <Nav.Link as={NavLink} to="/users">
+              <FontAwesomeIcon icon={faUsers} />
+            </Nav.Link>
+          )}
+
+          {role === "user" && (
+            <Nav.Link as={NavLink} to="/profile">
+              <FontAwesomeIcon icon={faUser} />
+            </Nav.Link>
+          )}
+
           <Nav.Link>
-            <Link
-            Style={{}}
-              to="/signUp"
-              
-            >
-              SignUp/In
-            </Link>
+            {role ? (
+          <Link to="/"
+          onClick={handleSignOut} 
+          style={{
+          borderRight: "none",
+          border: "3px solid grey",
+          borderRadius: "10px",
+          marginLeft: "190px",
+          padding: "8px",
+          }} 
+        >
+          Sign Out
+        </Link> 
+        ):(
+          <Link
+            to="/signUp"
+            style={{
+              borderRight: "none",
+              border: "3px solid grey",
+              borderRadius: "10px",
+              marginLeft: "190px",
+              padding: "8px",
+            }}>
+            Sign Up/In
+          </Link> 
+          )}
           </Nav.Link>
         </Nav>
       </Container>
