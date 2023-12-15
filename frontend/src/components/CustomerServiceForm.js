41e5d890edb0f0668bb3e54ac0900a6c04c4a8b3
emansarahafi <emansarahafi@gmail.com>
2023-12-15 19:36:46 +0100

    ...

--- frontend/src/components/CustomerServiceForm.js
+++ frontend/src/components/CustomerServiceForm.js
@@ -1,11 +1,13 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { MDBBtn, MDBContainer, MDBCard, MDBCardBody, MDBInput } from "mdb-react-ui-kit";
 import axios from "axios";
 
 function CustomerServiceForm() {
     const url = "http://localhost:8022/api/customerservices"; // Update the API endpoint for customer service
     const [customerInquiry, setCustomerInquiry] = useState({ name: "", email: "", inquiry: "" });
-    
+    const navigate = useNavigate();
+
     const handleChange = (e) => {
         setCustomerInquiry({ ...customerInquiry, [e.target.id]: e.target.value });
     };
@@ -17,6 +19,8 @@ function CustomerServiceForm() {
             .then((response) => {
                 console.log(response.data);
                 // Handle response...
+                alert("Request submitted! We will get back to you as soon as possible.");
+                navigate("/");
             })
             .catch((error) => {
                 console.error("There was an error!", error);
