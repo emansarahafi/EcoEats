ad3c07b9c9a515ef248fc644f065e1474c8f63a1
emansarahafi <emansarahafi@gmail.com>
2023-12-21 17:58:46 +0100

    Fixed the search and filtering issues.

--- frontend/src/components/ListOfRestaurants.js
+++ frontend/src/components/ListOfRestaurants.js
@@ -1,19 +1,17 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import RestaurantCardItem from "./RestaurantCardItem";
 import SearchBar from "./SearchBar";
 import axios from "axios";
 import "@picocss/pico";
-import { Navigate } from "react-router-dom"; 
 import { getUserRole } from "./UserRole";
 
 export default function ListOfRestaurants() {
   const [restaurants, setRestaurants] = useState([]);
   const [filteredRestaurants, setFilteredRestaurants] = useState([]);
-  const [locationfilteredRestaurants, setLocationFilteredRestaurants] = useState([]);
-  const [searchfilteredRestaurants, setSearchFilteredRestaurants] = useState([]);
   const [uniqueLocations, setUniqueLocations] = useState([]);
   const [minRatingFilter, setMinRatingFilter] = useState(0);
-  const [isSearchClicked, setIsSearchClicked] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [locationFilter, setLocationFilter] = useState('');
   const role = getUserRole();
 
   useEffect(() => {
@@ -30,8 +28,14 @@ export default function ListOfRestaurants() {
   }, []);
 
   useEffect(() => {
-    setFilteredRestaurants(restaurants);
-  }, [restaurants]);
+    setFilteredRestaurants(
+      restaurants.filter((restaurant) => 
+        restaurant.name.toLowerCase().includes(searchTerm.toLowerCase()) &&
+        (locationFilter === '' || restaurant.location === locationFilter) &&
+        restaurant.rating >= minRatingFilter
+      )
+    );
+  }, [restaurants, searchTerm, locationFilter, minRatingFilter]);
 
   useEffect(() => {
     const locations = restaurants.map((restaurant) => restaurant.location);
@@ -39,93 +43,42 @@ export default function ListOfRestaurants() {
     setUniqueLocations(uniqueLocations);
   }, [restaurants]);
 
-  const handleSearch = useCallback((searchTerm) => {
-    const newFilteredRestaurants = restaurants.filter((restaurant) =>
-      restaurant.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-
-    setSearchFilteredRestaurants(newFilteredRestaurants);
-    setIsSearchClicked(true);
-  }, [restaurants]);
-
-  useEffect(() => {
-    if (isSearchClicked) {
-      const combinedFilteredRestaurants = locationfilteredRestaurants.filter((restaurant) =>
-        searchfilteredRestaurants.includes(restaurant)
-      );
-
-      setFilteredRestaurants(combinedFilteredRestaurants);
-    }
-  }, [locationfilteredRestaurants, searchfilteredRestaurants, isSearchClicked]);
-
-  const handleLocationFilter = useCallback((location) => {
-    if (location === "") {
-      setLocationFilteredRestaurants(restaurants);
-    } else {
-      const newFilteredRestaurants = restaurants.filter(
-        (restaurant) => restaurant.location === location
-      );
-      setLocationFilteredRestaurants(newFilteredRestaurants);
-    }
-  }, [restaurants]);
-
-  const handleRatingFilter = useCallback((minRating) => {
-    if (minRating === "") {
-      setMinRatingFilter(0);
-    } else {
-      setMinRatingFilter(parseFloat(minRating));
-    }
-  }, []);
-
-  useEffect(() => {
-    const newFilteredRestaurants = filteredRestaurants.filter(
-      (restaurant) => restaurant.rating >= minRatingFilter
-    );
-    setFilteredRestaurants(newFilteredRestaurants);
-  }, [minRatingFilter]);
-
-  if (role === "admin") {
-    return <Navigate to="/users" /> 
-  }
-
   return (
     <div className="container">
       <div className="header">
         <h1>Restaurant Search</h1>
       </div>
-      <div className="filters">
-        <div className="search-bar">
-          <SearchBar onSearch={handleSearch} />
+      <div className="search-bar">
+        <SearchBar onSearch={(term) => setSearchTerm(term)} />
+      </div>
+      <div className="filter-dropdowns">
+        <div className="filter-dropdown">
+          <select
+            style={{ backgroundColor: "white" }}
+            id="location"
+            onChange={(e) => setLocationFilter(e.target.value)}
+          >
+            <option value="">All</option>
+            {uniqueLocations.map((location) => (
+              <option key={location} value={location}>
+                {location}
+              </option>
+            ))}
+          </select>
         </div>
-        <div className="filter-dropdowns">
-          <div className="filter-dropdown">
-            <select
-              style={{ backgroundColor: "white" }}
-              id="location"
-              onChange={(e) => handleLocationFilter(e.target.value)}
-            >
-              <option value="">All</option>
-              {uniqueLocations.map((location) => (
-                <option key={location} value={location}>
-                  {location}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="filter-dropdown">
-            <select
-              id="stars"
-              onChange={(e) => handleRatingFilter(e.target.value)}
-              style={{ backgroundColor: "white" }}
-            >
-              <option value="">All</option>
-              <option value="1">1 Star</option>
-              <option value="2">2 Stars</option>
-              <option value="3">3 Stars</option>
-              <option value="4">4 Stars</option>
-              <option value="5">5 Stars</option>
-            </select>
-          </div>
+        <div className="filter-dropdown">
+          <select
+            id="stars"
+            onChange={(e) => setMinRatingFilter(parseFloat(e.target.value))}
+            style={{ backgroundColor: "white" }}
+          >
+            <option value="">All</option>
+            <option value="1">1 Star</option>
+            <option value="2">2 Stars</option>
+            <option value="3">3 Stars</option>
+            <option value="4">4 Stars</option>
+            <option value="5">5 Stars</option>
+          </select>
         </div>
       </div>
       <div className="restaurant-list" style={{ display: "flex", justifyContent: "space-around" }}>
